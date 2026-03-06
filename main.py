390908ef39b49efc84f9eddde547d2576f0cf4a5
jperparas <j.s.perparas@gmail.com>
2026-03-05 23:57:19 -0800

    add textwrap to test output

--- main.py
+++ main.py
@@ -8,13 +8,11 @@ st.set_page_config(page_title="Text Assembler", layout="centered")
 st.title("Prompt Designer")
 st.info("PARTS")
 
-# Initialize session state for the result
 if "final_text" not in st.session_state:
     st.session_state.final_text = ""
 
 parts = ["Persona","Aim","Recipients","Theme","Structure"]
 with st.form("input_form"):
-    # Create 5 inputs dynamically using a list comprehension or simple loop
 
     inputs = []
     for part in parts:
@@ -24,10 +22,9 @@ with st.form("input_form"):
     submit = st.form_submit_button("➤",use_container_width=True)
 
 if submit:
-    # Filter out empty strings and join with double newlines
     st.session_state.final_text = groq.generate(generate_meta_prompt(inputs))
 
 if st.session_state.final_text:
     st.subheader("Result")
-    st.code(st.session_state.final_text, language=None)
+    st.code(st.session_state.final_text, language=None, wrap_lines=True)
 
